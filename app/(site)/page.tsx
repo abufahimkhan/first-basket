017ba01e491c85516e887210108b2a0d2469887f
HasanAkhonda <mdmahmudhasanakhonda@gmail.com>
2025-05-06 21:37:46 +0600

    Card feature section integrated

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,17 @@
-import { Metadata } from "next";
-import Hero from "@/components/Hero";
-import Brands from "@/components/Brands";
-import Feature from "@/components/Features";
 import About from "@/components/About";
-import FeaturesTab from "@/components/FeaturesTab";
-import FunFact from "@/components/FunFact";
-import Integration from "@/components/Integration";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import Contact from "@/components/Contact";
 import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQ";
+import Feature from "@/components/Features";
+import FeaturesTab from "@/components/FeaturesTab";
+import FunFact from "@/components/FunFact";
+import Hero from "@/components/Hero";
+import Integration from "@/components/Integration";
 import Pricing from "@/components/Pricing";
-import Contact from "@/components/Contact";
-import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "First Basket E-Commerce",
---
+++ components/assets/index.ts
@@ -0,0 +1,9 @@
+import nailPolish from './cosmetics/polish.jpg'
+
+
+export const cosmeticsImage={
+    nailPolish,
+}
+export const groceryImage={
+    nailPolish,
+}
